b132fec0b3dbd948615653471867c5a352e3335e
KKaviya1 <kaviyagandhi136@gmail.com>
2023-07-11 10:46:11 +0530

    my commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conappswitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cCode;
+            string lang;
+            Console.WriteLine("enter country code");
+            cCode = Console.ReadLine();
+            switch (cCode)
+            {
+                case "uk":
+                    {
+                        lang = "Dari,Pashto";
+                        break;
+                    }
+                case "in":
+                    {
+                        lang = "hindi,english,tamil,telugu";
+                        break;
+                    }
+                case "uae":
+                    {
+                        lang = "arabic";
+                        break;
+
+                    }
+
+                default:
+                    {
+                        Console.WriteLine($"sorry!! you country {cCode} is not in ourdb");
+                        lang = "not found!!!";
+                        break;
+                    } 
+            }
+            Console.WriteLine("country code " + cCode + "languages(s)" + lang);
+            Console.ReadKey();
+        } } 
+}
